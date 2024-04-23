37f84896012e6402b5f9cf64a737e0e3ce86be5e
Finnigan Johnson <soul_sap0t@icloud.com>
2024-04-22 20:44:59 -0700

    Added basic speech recognition

---
+++ main.py
@@ -0,0 +1,10 @@
+import speech_to_text
+
+def main():
+    # Your code here
+    # Example usage
+    text = speech_to_text.record_text()
+    print(text)
+
+if __name__ == "__main__":
+    main()
---
+++ speech_to_text.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+
+def record_text():
+    # Record audio
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=0.2)
+        print("Hello, I am Baymax, your personal healthcare companion. How can I help you?")
+        audio = r.listen(source)
+
+    # Convert audio to text
+    try:
+        text = r.recognize_google(audio)
+        print("You said: " + text)
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+    return text
