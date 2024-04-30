5d2b333d894ed66b3a7afca2095fc9872555f6d5
Finnigan Johnson <soul_sap0t@icloud.com>
2024-04-29 20:42:10 -0700

    completed tts and added conda env file

--- main.py
+++ main.py
@@ -5,16 +5,11 @@ import text_to_speech
 
 def main():
     messages = []
-    
     while(1):
         text = speech_to_text.record_text()
         messages.append({"role": "user", "content": text}) # TODO: update for barvis (system, content)
         response = send_to_chatGPT.openAI_api_request(messages)
-        # TODO: text_to_speech.speak_text(response)
-
-        # temps
-        print("THIS IS THE TEXT: " + text + '\n')
-        print("THIS IS THE RESPONSE: " + response)
+        text_to_speech.speak_text(response)
 
         # TODO: rn it crashes fast when it recieves no more user input
 
--- text_to_speech.py
+++ text_to_speech.py
@@ -2,7 +2,6 @@ import pyttsx3
 
 # Function for converting text to speech
 def speak_text(command):
-    # Initialize the engine
     engine = pyttsx3.init()
     engine.say(command)
     engine.runAndWait()
